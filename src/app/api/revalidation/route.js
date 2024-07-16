c944b20c3dc56ec826fb535afce4a526120f68bb
Tillmann Severin <tillmann.severin@posteo.de>
2024-07-16 20:35:02 +0200

    update branch

---
+++ src/app/api/revalidation/route.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { revalidateTag } from "next/cache";
+
+export async function POST(request) {
+    const requestHeaders = new Headers(request.headers);
+    const secret = requestHeaders.get("x-vercel-reval-key");
+
+    if (secret !== process.env.CONTENTFUL_REVALIDATE_SECRET) {
+        return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
+    }
+
+    revalidateTag("articles");
+    revalidateTag("landingPage");
+
+    return NextResponse.json({ revalidated: true, now: Date.now() });
+}
+
