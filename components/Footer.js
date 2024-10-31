4e37afe7a0ccad71b8f25efc979f18e4c81ae739
Tillmann Severin <tillmann.severin@posteo.de>
2024-10-31 14:42:14 +0100

    change mail in footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
         <div className="flex pt-4 text-lg justify-between sm:text-xl">
             <Link href="/impressum" className="no-underline font-roboto-condensed font-bold text-sm sm:text-xl"><p>IMPRESSUM</p></Link>
             <div className="flex">
-                <Link href="mailto:ahoj@schreibengegendienormen.de" className="font-roboto-condensed font-bold text-sm sm:text-xl no-underline pl-4"><p>E-MAIL</p></Link>
+                <Link href="mailto:hello@schreibengegendienormen.de" className="font-roboto-condensed font-bold text-sm sm:text-xl no-underline pl-4"><p>E-MAIL</p></Link>
                 <Link href="https://www.instagram.com/schreibengegendienormen/" target="_blank" className="no-underline font-roboto-condensed font-bold text-sm sm:text-xl pl-2 sm:pl-4"><p>INSTAGRAM</p></Link>
                 <Link href="https://www.facebook.com/schreibengegendienormen" target="_blank" className="no-underline font-roboto-condensed font-bold text-sm sm:text-xl px-4"><p>FACEBOOK</p></Link>
             </div>
